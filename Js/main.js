f5c1a25eee036b1f329ffd2ceeb9c89dc0592fad
Pongerard Etienne <etiennepongerard@MBP-de-etienne.lan>
2025-08-16 17:39:38 +0200

    modification de la page d'accueil

--- Js/main.js
+++ Js/main.js
@@ -8,24 +8,28 @@ document.addEventListener("DOMContentLoaded", function () {
     // gestion du texte de présentations des différents sites en fonctions de l'image affichée
     let txtPresentationSite = [
         "voici un site web réalisé en partenariat avec une équipe d'étudiants en journalisme.Ce site a été développé en PHP avec Twig pour l'affichage.Il est fourni avec un back office permettant aux journalistes d’intégrer leurs articles comme ils le souhaitent. Le code est disponible sur mon GitHub",
-        "Ce site à était réalisé en partenariat avec la maison science alsace. Il a était développé avec symphonie pour la base de donnée, twig pour l'affichage et javascript pour les interractions. Il s'agit d'un jeu pour apprendre au plus jeune le cycle de l'eau. Je vous conseille d'aller tester le jeu par vous même pour vous rendre mieux compte du travailler réalisé ici. Ce jeu est aussi fournis avec un back office afin de permettre à la maisosn science alsace de modifier le contenu du jeu. Le code est disponible sur mon GitHub",
+        "Ce site a été  réalisé en partenariat avec la maison science alsace. Il a était développé avec symphonie pour la base de donnée, twig pour l'affichage et javascript pour les interractions. Il s'agit d'un jeu pour apprendre au plus jeune le cycle de l'eau. Je vous conseille d'aller tester le jeu par vous même pour vous rendre mieux compte du travailler réalisé ici. Ce jeu est aussi fournis avec un back office afin de permettre à la maisosn science alsace de modifier le contenu du jeu. Le code est disponible sur mon GitHub",
         "Ce site à était réalisé pour une entreprise de location d'appartement saisionnier. J'ai réalisé l'entierté des pages sur wordpress, majoritairement à l'aide d'élementor. Le site est responsive et s'adapte à tous les écrans. Il est aussi optimisé pour le référencement naturel. Le site est en ligne et vous pouvez le visiter pour voir le résultat final. Le code n'est pas disponible car il s'agit d'un site client.",
+        "Ce site a été réaliser pour un centre de formation. Il a était réaliser sur wordpress, majoritairement à l'aide d'élementor. Le site est responsive et s'adapte à tous les écrans. Il est aussi optimisé pour le référencement naturel. Le site est en ligne et vous pouvez le visiter pour voir le résultat final. Le code n'est pas disponible car il s'agit d'un site client."
     ];
     // liens pour les différents sites
     let LienSiteWeb = [
         "https://cuej.info/mini-sites/europe2024/visitor.php?page=theme",
         "https://decrypteau.maurerc.etu.mmi-unistra.fr/",
-        "https://edaee-immo.fr/"
+        "https://edaee-immo.fr/",
+        "https://cafes-formations.re/"
+
     ];
     let TitreSite = [
         "Site web CUEJ",
         "Site web Décrypteau",
-        "Site web EdaeeImmo"
+        "Site web EdaeeImmo",
+        "Site web Cafés Formations"
     ];
 
     document.getElementById("btnNext").addEventListener("click", function () {
         index++;
-        if (index > 2) index = 0; // Reset index if it exceeds
+        if (index > LienSiteWeb.length-1) index = 0; // Reset index if it exceeds
         document.getElementById("textSlide").innerText = txtPresentationSite[index];
         TitreSiteElement.innerText = TitreSite[index];
         btnVisite.href = LienSiteWeb[index];
@@ -33,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("btnPrev").addEventListener("click", function () {
         index--;
-        if (index < 0) index = 2; // Reset index if it goes below 0
+        if (index < 0) index = (LienSiteWeb.lenthg-1); // Reset index if it goes below 0
         document.getElementById("textSlide").innerText = txtPresentationSite[index];
         TitreSiteElement.innerText = TitreSite[index];
         btnVisite.href = LienSiteWeb[index];
